66b0d34c1d6d0ca732515f88a2092daf0dc97c6b
Melker Tublén <mt223xr@student-lnu.se>
2025-11-13 10:44:56 +0100

    Add weather search feature with OpenWeather API and input field

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+
+  const API_KEY = "34f6f7a51fd76d1f101ac7a75a85144f";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${API_KEY}`;
+
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+      setLocation("");
+    }
+  };
+  return (
+    <div className="app">
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Ange plats..."
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyDownCapture={searchLocation}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
